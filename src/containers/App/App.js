c5bcd5de7945348b5438f2b4b6819fb8f9137fc9
Caleb Everett <caleb@careercalifornia.edu>
2015-11-09 15:14:16 -0800

    Revert "still trying to fix"
    This reverts commit 57caf343cb62c7c13fb58bf2334aa4ba89cdca0d.

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -7,7 +7,6 @@ import { isLoaded as isAuthLoaded, load as loadAuth, logout } from 'redux/module
 import { InfoBar } from 'components';
 import { pushState } from 'redux-router';
 import config from '../../config';
-import { Button } from 'react-toolbox';
 
 const NavbarLink = ({to, className, component, children}) => {
   const Comp = component || Link;
@@ -98,7 +97,6 @@ export default class App extends Component {
           {this.props.children}
         </div>
         <InfoBar/>
-        <Button label="Hello world" kind="raised" accent />
 
         <div className="well text-center">
           Have questions? Ask for help <a
